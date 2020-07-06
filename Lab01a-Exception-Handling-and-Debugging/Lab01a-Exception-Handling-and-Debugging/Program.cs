4aa2d278b3d8d9051f7fc5657734ba0284a7e999
Peyton-Cysewski <github.peyton.cysewski@gmail.com>
2020-07-06 12:49:47 -0700

    Main function set up

---
+++ Lab01a-Exception-Handling-and-Debugging/Lab01a-Exception-Handling-and-Debugging/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Lab01a_Exception_Handling_and_Debugging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                StartSequence();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("It appears you made a mistake with one of your entries.");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("This program has completed.")
+            }
+        }
+
+
+    }
+}
